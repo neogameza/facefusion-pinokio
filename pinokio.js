d65f4cd1bba81d7604f0d597232032de7d8c9816
neogameza <109970073+neogameza@users.noreply.github.com>
2026-09-21 08:15:26 +0700

    Update pinokio.js

--- pinokio.js
+++ pinokio.js
@@ -1,7 +1,7 @@
 module.exports =
 {
 	version: '5.0',
-	title: 'FaceFusion 3.8.3',
+	title: 'FaceFusion 3.9.0',
 	description: 'Industry leading face manipulation platform',
 	icon: 'facefusion.png',
 	menu: require(__dirname + '/menu.js')
